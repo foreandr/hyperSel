f05c46be2b0bd21c4ea077c6e892b1c55e91b7ec
foreandr <foreandr@gmail.com>
2024-06-09 23:50:11 -0400

    tst init commit

---
+++ hyperSel/main.py
@@ -0,0 +1,2 @@
+def test_func(msg):
+    print(msg)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hyperSel",
+    version=1.1,
+    packages=find_packages(),
+    install_requires=[
+        # selenium
+        # seleinum web manages
+    ]
+)
+
