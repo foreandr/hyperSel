bcddd4da438493b84d53ac067cbd850b96233097
foreandr <foreandr@gmail.com>
2024-06-10 20:46:49 -0400

    git update

---
+++ hyperSel/test.py
@@ -0,0 +1,4 @@
+import hyperSel
+
+if __name__ == "__main__":
+    pass 
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperSel",
-    version=1.1,
+    version=1.2,
     packages=find_packages(),
     install_requires=[
-        # selenium
-        # seleinum web manages
+        "selenium==4.21.0",
+        "beautifulsoup4==4.12.3",
+        "webdriver-manager==4.0.1"
     ]
 )
-
