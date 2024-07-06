fec7f920786a4cc7eab488b75bdc42bdce45141a
foreandr <foreandr@gmail.com>
2024-07-06 17:32:08 -0400

    4

--- hyperSel/hyperSel.py
+++ hyperSel/hyperSel.py
@@ -1,4 +1,3 @@
-# try again
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperSel",
-    version=2.8,
+    version=2.9,
     packages=find_packages(),
     install_requires=[
         "selenium==4.21.0",
