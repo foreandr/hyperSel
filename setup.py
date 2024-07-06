af5ef271e563265cc4777534d0a6bf2d64bbf84d
foreandr <foreandr@gmail.com>
2024-07-06 17:28:55 -0400

    committing

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperSel",
-    version=2.5,
+    version=2.6,
     packages=find_packages(),
     install_requires=[
         "selenium==4.21.0",
         "beautifulsoup4==4.12.3",
         "webdriver-manager==4.0.1",
         "lxml==5.2.2",
+        "requests==2.32.3",
     ],
     include_package_data=True, 
 )
