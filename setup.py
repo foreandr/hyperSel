0cbfe99102b0bca2c3fb0a2df5f2bac5a2e92b9d
foreandr <foreandr@gmail.com>
2024-07-25 15:35:53 -0400

    again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperSel",
-    version=2.24,
+    version=2.25,
     packages=find_packages(),
     install_requires=[
         "selenium",
