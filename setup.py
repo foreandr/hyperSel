2d3a2c2e050dcb35e77e40e5c2a4fdec06c5eb8b
foreandr <foreandr@gmail.com>
2024-07-25 12:56:50 -0400

    removed htuk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperSel",
-    version=2.17,
+    version=2.18,
     packages=find_packages(),
     install_requires=[
         "selenium",
