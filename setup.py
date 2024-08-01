02c14a5e5e7ae4a2016c140b890b6fba0892ec26
foreandr <foreandr@gmail.com>
2024-08-01 15:31:45 -0400

    selenium fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperSel",
-    version=2.28,
+    version=2.29,
     packages=find_packages(),
     install_requires=[
         "selenium",
