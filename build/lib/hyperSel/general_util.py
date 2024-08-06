eec3139b7ed788615a58e205835b6f39fe606d17
foreandr <foreandr@gmail.com>
2024-08-06 09:44:01 -0400

    finish general util

---
+++ build/lib/hyperSel/general_util.py
@@ -0,0 +1,45 @@
+import subprocess
+
+def get_ram_percentage():
+    return int(str(psutil.virtual_memory().percent).split(".")[0])
+
+def ram_under_threshold(threshold):
+    if get_ram_percentage() <= threshold:
+        return True
+    return False
+
+def list_chrome_instances():
+    chrome_processes = []
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'] == 'chrome.exe':
+            chrome_processes.append(process.info)
+    return chrome_processes
+
+def kill_chrome_instances():
+    try:
+        # Run the command to kill Chrome instances
+        result = subprocess.run(['taskkill', '/F', '/IM', 'chrome.exe'], capture_output=True, text=True, check=True)
+        # print("Command output:", result.stdout)
+        # print("Command error (if any):", result.stderr)
+        print("KILLED ALL CHROME INSTANCES")
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+        print("Error output:", e.stderr)
+    except FileNotFoundError:
+        print("The 'taskkill' command was not found. Is it available on your system?")
+        
+def kill_process_by_pid(pid):
+    try:
+        # Run the command to kill the process with the given PID
+        result = subprocess.run(['taskkill', '/F', '/PID', str(pid)], capture_output=True, text=True, check=True)
+        # Print the command output and error (if any)
+        print("Command output:", result.stdout)
+        print("Command error (if any):", result.stderr)
+        print(f"Killed process with PID: {pid}")
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+        print("Error output:", e.stderr)
+    except FileNotFoundError:
+        print("The 'taskkill' command was not found. Is it available on your system?")
+    except Exception as e:
+        print(f"An error occurred while trying to kill process with PID: {pid} - {e}")    
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperSel",
-    version=2.31,
+    version=2.32,
     packages=find_packages(),
     install_requires=[
         "selenium",
