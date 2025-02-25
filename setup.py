0fc2fb7812d3561d45b72e592c5ec49a63bbad90
foreandr <foreandr@gmail.com>
2025-02-24 19:06:56 -0500

    making webweb

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 # Setup configuration
 setup(
     name="hyperSel",
-    version=5.4,
+    version=5.5,
     author="foreandr",  # Your name or username
     author_email="foreandr@gmail.com",  # Your email
     description="A Python-based web automation and data scraping framework",  # Short description
