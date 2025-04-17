d4aa2449020990200d27528b3cd25f5ed24151c8
foreandr <foreandr@gmail.com>
2025-04-17 15:59:57 -0400

    compiler

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 # Setup configuration
 setup(
     name="hypersel",
-    version=6.1,
+    version=6.2,
     author="foreandr",  # Your name or username
     author_email="foreandr@gmail.com",  # Your email
     description="A Python-based web automation and data scraping framework",  # Short description
